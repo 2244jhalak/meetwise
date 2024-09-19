abdaa7a52ecf0c76a6fa67d6f2f56f3dee5cdec2
HIMU005 <hashanuzzaman99@gamil.com>
2024-09-19 20:22:18 +0600

    done the try meet section and footer section

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
-
-
 import AutoRecord from "./components/Homepage/AutoRecord";
 import Banner from "./components/Homepage/Banner";
+import TryMeet from "./components/Homepage/TryMeet";
 
 export default function Home() {
   return (
     <div>
-
       <Banner />
       <AutoRecord></AutoRecord>
+      <TryMeet />
     </div>
   );
 }
