ebb46dbafa4c95fb03d912542b4cf746ea1da0ff
oishee <oisheekhan90@gmail.com>
2024-09-19 20:30:02 +0600

    form section complete

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ContactHeader from '../components/Contactpage/ContactHeader';
 import Faq from '../components/Contactpage/Faq';
+import Form from '../components/Contactpage/Form';
 
 const page = () => {
     return (
         <div className='bg-[#F8ECFF]'>
        <ContactHeader></ContactHeader>
        <Faq></Faq>
+       <Form></Form>
         </div>
     );
 };
