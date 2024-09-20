2266631ec70cee6ab7d1d7685b7a2e0208e9a166
Anamul029 <anamhaque.5101@gmail.com>
2024-09-20 15:32:02 +0600

    adding about us section

---
+++ src/app/components/AboutUs/AboutHeader.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const AboutHeader = () => {
+    return (
+        <div className='container mx-auto'>
+            <div
+                className="hero h-[300px] md:h-[450px] rounded-sm"
+                style={{
+                    backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.7)),url('https://st.depositphotos.com/1038076/4908/i/450/depositphotos_49080337-stock-photo-about-us.jpg')`,
+                    backgroundSize: 'cover',        // Ensure the image covers the container
+                    backgroundPosition: 'center',   // Center the image
+                    backgroundRepeat: 'no-repeat'
+                }}
+            >
+                <div className="hero-overlay bg-opacity-70"></div>
+                <div className="hero-content text-center text-neutral-content">
+                    <div className="my-auto">
+                        <h1 className=" text-4xl md:text-6xl w-full pt-20  text-white font-extrabold font-raleway mb-4" >About Us</h1>
+                        <p className="mt-2 md:mt-5 text-base md:text-xl w-full  text-gray-200 p-4  font-raleway md:mb-4" >
+                            Welcome to Meetwise, your go-to platform for effortless and efficient meeting scheduling. We are a team of tech enthusiasts, committed to simplifying the way individuals and businesses schedule their meetings. In today’s fast-paced world, coordinating calendars should be a breeze, and that’s exactly what we aim to provide.
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AboutHeader;
