922b5bd7aa1cadf817aa2f09274818fc0d2f04c2
Jhalak <stdjhalak@gmail.com>
2024-09-25 08:53:03 +0600

    language ditector added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 import ContactHeader from '../components/Contactpage/ContactHeader';
 import Faq from '../components/Contactpage/Faq';
 import Form from '../components/Contactpage/Form';
+import Navbar from '../components/Homepage/Navbar/Navbar';
+import Footer from '../components/Shared/Footer';
 
 const page = () => {
     return (
         <div className='bg-[#F8ECFF]'>
+        <div className="bg-[#f7ecff]">
+          <div className="container mx-auto">
+            <Navbar></Navbar>
+          </div>
+       </div>
        <ContactHeader></ContactHeader>
        <Faq></Faq>
        <Form></Form>
+       <Footer></Footer>
         </div>
     );
 };
