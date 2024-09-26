ce9e7888e696fa3e99d6ffa8d4402c47abb71eb1
HIMU005 <hashanuzzaman99@gamil.com>
2024-09-26 16:36:29 +0600

    fixed the color palate with the new color palate of landing page

--- src/app/components/dashboard/NavigationDash.jsx
+++ src/app/components/dashboard/NavigationDash.jsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 const NavigationDash = ({ label, address, icon: Icon }) => {
     return (
-        <Link href={address}
-            end
-            className="flex w-full items-center px-4 py-2 mt-5 text-gray-600 hover:bg-[#7B61FF]  hover:text-gray-700 transition-colors duration-300 transform"
-        >
-            <Icon className='w-5 h-5' />
+        <div className="group font-raleway">
+            <Link href={address}
+                end
+                className="flex w-full items-center px-4 py-2 mt-5 text-black hover:bg-[#183C4B] hover:text-white transform transition-all duration-500 ease-in font-raleway"
+            >
+                <Icon className='w-5 h-5' />
 
-            <span className='mx-4 font-medium text-black'>{label}</span>
-        </Link>
+                <span className='mx-4 font-medium text-black group-hover:text-white'>{label}</span>
+            </Link>
+        </div>
     );
 };
 
