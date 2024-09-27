135b936b4bebe8ea5573d893d338a141fbdbd33b
oishee <oisheekhan90@gmail.com>
2024-09-27 11:03:54 +0600

    sign up page design

--- src/app/components/SocialLogin/SocialLogin.jsx
+++ src/app/components/SocialLogin/SocialLogin.jsx
@@ -21,7 +21,7 @@ const SocialLogin = () => {
     
     return (
         <div>
-           <button className='flex flex-row items-center space-x-2 p-2 bg-[#4c8198] hover:bg-[#183c4b] transition-all duration-500 ease-in font-raleway rounded-md' onClick={()=>handleSignIn("google")}>
+           <button className='flex flex-row items-center space-x-2 p-2 border border-blue-900 bg-slate-100 hover:bg-[#183c4b] transition-all duration-500 ease-in font-raleway rounded-md' onClick={()=>handleSignIn("google")}>
     <Image
         src="/banner/icong.png"
         alt="Google Icon"
@@ -29,7 +29,7 @@ const SocialLogin = () => {
         height={30} // Set a fixed height for the icon
         className="object-contain" // Ensure the image fits within the container
     />
-    <p className="font-semibold  text-white">Continue With Google</p>
+    <p className="font-semibold text-black  hover:text-white transition-all duration-500 ease-in ">Continue With Google</p>
 </button>
 
         </div>
