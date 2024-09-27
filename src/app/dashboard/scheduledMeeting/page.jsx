b062bf0206ec0c20d121a5736d8ed41c24971e89
Anamul029 <anamhaque.5101@gmail.com>
2024-09-27 11:41:10 +0600

    adding shedulied ui

--- src/app/dashboard/scheduledMeeting/page.jsx
+++ src/app/dashboard/scheduledMeeting/page.jsx
@@ -1,9 +1,12 @@
+import Meeting_Tab from '@/app/components/dashboard/Scheduled_Meeting/Meeting_Tab';
 import React from 'react';
 
 const page = () => {
     return (
         <div>
-            i am scheduledMeeting
+            <h1 className='text-2xl my-6 font-semibold'>Scheduled Meeting</h1> 
+            <hr/>
+            <Meeting_Tab></Meeting_Tab>
         </div>
     );
 };
