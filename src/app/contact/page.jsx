ebb0f377fcf961927a44494a4ef7340eb96ea2c5
oishee <oisheekhan90@gmail.com>
2024-10-04 11:00:57 +0600

    update contact header section

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -8,7 +8,7 @@ import Footer from '../components/Shared/Footer';
 const page = () => {
     return (
         <div className=" container mx-auto bg-[url('/banner/dr3.jpg')] bg-center min-h-screen">
-        <div className=" container mx-auto bg-white/10 backdrop-blur-md backdrop-opacity-70">
+        <div className=" container mx-auto bg-black/10 backdrop-blur-md backdrop-opacity-70">
             <div className="container hero-overlay bg-opacity-50 mx-auto">
               <Navbar></Navbar>
             </div>
