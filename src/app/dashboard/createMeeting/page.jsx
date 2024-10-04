48bae45e15cc4e3ba9dafbbe381275c88a707ca0
HIMU005 <hashanuzzaman99@gamil.com>
2024-10-04 11:47:25 +0600

    fixed the conflict with create meeting route

--- src/app/dashboard/createMeeting/page.jsx
+++ src/app/dashboard/createMeeting/page.jsx
@@ -10,16 +10,3 @@ const page = () => {
 };
 
 export default page;
-import LeftPart from "@/app/components/CreateMeeting/LeftPart";
-
-const page = () => {
-    return (
-        <div className="">
-            {/* left part  */}
-            <LeftPart />
-            {/* right part */}
-        </div>
-    );
-};
-
-export default page;
