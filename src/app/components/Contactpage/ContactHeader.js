fc24055fab32389adca07d0fc5d38a5fff1a1071
oishee <oisheekhan90@gmail.com>
2024-10-04 11:00:57 +0600

    update contact header section

--- src/app/components/Contactpage/ContactHeader.js
+++ src/app/components/Contactpage/ContactHeader.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const ContactHeader = () => {
   return (
-    <div className=' container mx-auto bg-white/10 backdrop-blur-md backdrop-opacity-70'>
+    <div className=' container mx-auto bg-black/10 backdrop-blur-md backdrop-opacity-70'>
       <div
-        className="hero h-[300px] md:h-[450px] rounded-sm"
+        className="hero h-[300px] md:h-[500px] rounded-sm"
         style={{
           backgroundImage: `url('')`,
           backgroundSize: 'cover',        // Ensure the image covers the container
@@ -15,7 +15,7 @@ const ContactHeader = () => {
         <div className="hero-overlay bg-opacity-50"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="my-auto">
-            <h1 className=" text-4xl md:text-6xl w-full pt-20  text-white font-extrabold font-raleway mb-4" >Contact Us</h1>
+            <h1 className=" text-4xl md:text-6xl w-full pt-7  text-white font-extrabold font-raleway mb-4" >Contact Us</h1>
             <p className="mt-2 md:mt-5 text-base md:text-xl w-full  text-gray-200  font-raleway md:mb-4" >
               Have any questions or feedback? We are here to help! Reach out to us through our contact form, and we will get back to you as soon as possible.
             </p>
