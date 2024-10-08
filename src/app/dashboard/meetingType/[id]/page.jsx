03d95487deb25e93a0f85166a0e63ea0d9ea6389
Jhalak <stdjhalak@gmail.com>
2024-10-08 11:28:06 +0600

    resolve

---
+++ src/app/dashboard/meetingType/[id]/page.jsx
@@ -0,0 +1,23 @@
+import MeetingDetails from '@/app/components/dashboard/meetingType/MeetingDetails';
+import React from 'react';
+
+const Page = async ({ params }) => {
+    const { id } = params; // Get the dynamic id from URL
+
+    // Fetch meeting details based on the id
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/dashboard/createMeeting/api/${id}`);
+    if (!res.ok) {
+        throw new Error('Failed to fetch meeting details');
+    }
+    const meetingDetails = await res.json();
+
+    return (
+        <div>
+            <h1>Meeting Type</h1>
+            <MeetingDetails meetingDetails={meetingDetails} />
+        </div>
+    );
+};
+
+export default Page;
+
