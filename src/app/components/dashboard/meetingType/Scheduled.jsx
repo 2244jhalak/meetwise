87ff65954c06127a433e46cd55a085ba8d0b7af8
HIMU005 <hashanuzzaman99@gamil.com>
2024-10-16 20:53:08 +0600

    fixed the meeting type route backend  env

--- src/app/components/dashboard/meetingType/Scheduled.jsx
+++ src/app/components/dashboard/meetingType/Scheduled.jsx
@@ -14,7 +14,7 @@ const Scheduled = () => {
     useEffect(() => {
         const fetchUser = async () => {
             try {
-                const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/dashboard/meetingType/api/${session?.data?.user?.email}`);
+                const response = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_URL}/dashboard/meetingType/api/${session?.data?.user?.email}`);
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
@@ -56,32 +56,32 @@ const Scheduled = () => {
                         <div className='rounded-lg border border-green-500 bg-black text-white  shadow-2xl ' key={meet._id}>
                             <div className='bg-green-700 w-full h-[30px] rounded-t-lg'></div>
                             <div className='p-8 space-y-4'>
-                            <h2 className='text-xl font-bold '>Event Name:<span className=''>{meet.eventName}</span></h2>
-                            <h2 className='text-xl font-bold '>Description: <span className='text-base font-medium'>{meet.eventName}</span> </h2>
-                            <div className='flex items-center justify-between'>
-                                <div className='flex items-center gap-2'>
-                                    <FaClock />
-                                    <p>{meet.duration}</p>
+                                <h2 className='text-xl font-bold '>Event Name:<span className=''>{meet.eventName}</span></h2>
+                                <h2 className='text-xl font-bold '>Description: <span className='text-base font-medium'>{meet.eventName}</span> </h2>
+                                <div className='flex items-center justify-between'>
+                                    <div className='flex items-center gap-2'>
+                                        <FaClock />
+                                        <p>{meet.duration}</p>
+                                    </div>
+                                    <div className='flex items-center gap-2'>
+                                        <FaLocationArrow />
+                                        <p>{meet.selected}</p>
+                                    </div>
                                 </div>
-                                <div className='flex items-center gap-2'>
-                                    <FaLocationArrow />
-                                    <p>{meet.selected}</p>
+                                <hr />
+                                <div className='flex justify-between'>
+                                    <div className='flex items-center gap-2 text-green-600 cursor-pointer' onClick={() => handleCopyLink(meet._id)}>
+                                        <FaCopy />
+                                        <p>Copy Link</p>
+                                    </div>
+                                    <button
+                                        className='text-orange-500 underline cursor-pointer'
+                                        onClick={() => handleViewDetails(meet._id)}
+                                    >
+                                        View Details
+                                    </button>
                                 </div>
                             </div>
-                            <hr />
-                            <div className='flex justify-between'>
-                                <div className='flex items-center gap-2 text-green-600 cursor-pointer' onClick={() => handleCopyLink(meet._id)}>
-                                    <FaCopy />
-                                    <p>Copy Link</p>
-                                </div>
-                                <button 
-                                    className='text-orange-500 underline cursor-pointer'
-                                    onClick={() => handleViewDetails(meet._id)}
-                                >
-                                    View Details
-                                </button>
-                            </div>
-                        </div>
                         </div>
                     ))
                 }
