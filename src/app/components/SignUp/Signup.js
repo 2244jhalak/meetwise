7398bfbc18672113b51e48b280176ed481417c87
Jhalak <stdjhalak@gmail.com>
2024-10-20 21:03:49 +0600

    Resolved conflicts and merged main into jhalak

--- src/app/components/SignUp/Signup.js
+++ src/app/components/SignUp/Signup.js
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import Image from "next/image";
 import { useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
