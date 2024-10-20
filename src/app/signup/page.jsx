fa2ce83c19393a313b1441c363c69e24fd6029c1
Jhalak <stdjhalak@gmail.com>
2024-10-21 01:19:47 +0600

    fix mone hoy

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Signup from '../components/SignUp/Signup';
 import Notification from '../components/Notification';
 
 const page = () => {
     return (
         <div className=" container mx-auto bg-[url('/banner/dr3.jpg')] bg-center min-h-screen">
-           <Signup></Signup>
+            <Suspense>
+            <Signup></Signup>
+            </Suspense>
+           
            <div className="container mx-auto">
                 <Notification></Notification>
            </div>
