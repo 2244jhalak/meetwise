84d2192bfeb3481170f190076fd8524176cf56c8
Jhalak <stdjhalak@gmail.com>
2024-10-21 01:17:09 +0600

    suspense

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+
+import React, { Suspense } from 'react';
 import SignIn from '../components/SignIn/SignIn';
 import Notification from '../components/Notification';
 
 const page = () => {
     return (
         <div className=" container mx-auto bg-[url('/banner/dr3.jpg')] bg-center min-h-screen">
+            <Suspense>
             <SignIn></SignIn>
+            </Suspense>
+            
             <div className="container mx-auto">
                  <Notification></Notification>
             </div>
