4de3a46e06ffae786b17ae27243bfd124b7b29ac
Anamul029 <anamhaque.5101@gmail.com>
2024-10-22 19:43:50 +0600

    change the design of  homepage all features section

--- src/app/dashboard/availability/page.jsx
+++ src/app/dashboard/availability/page.jsx
@@ -8,18 +8,16 @@ import PrivateRoute from '@/app/services/PrivateRoute';
 const Page = () => {
     
         return (
-<<<<<<< HEAD
-            <div className=' bg-[#4A4947] min-h-screen md:flex md:flex-row md:justify-between'>
-=======
             <PrivateRoute>
+            <div className=' bg-[#4A4947] min-h-screen md:flex md:flex-row md:justify-between'>
                 <div className=' bg-[#4A4947] md:flex md:flex-row md:justify-between'>
->>>>>>> 961ba096884bf398fcfe6536680398407835414f
                 <div className='md:w-1/4'>
                     <Sidebar />
                 </div>
                 <div className='container mx-auto md:w-3/4 p-4'>
                     <AvailabilityForm />
                 </div>
+            </div>
             </div>
             </PrivateRoute>
         );
