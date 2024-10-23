daa0f4c234339b8dcea94a97c0b1669f33c765f5
oishee <oisheekhan90@gmail.com>
2024-10-23 11:43:46 +0600

    dashboard design modify

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,39 +1,27 @@
 "use client";
-
-import Image from 'next/image';
 import Sidebar from '../components/dashboard/Sidebar';
 
 import Notification from '../components/Notification';
 import PrivateRoute from '../services/PrivateRoute';
 import { useSession } from 'next-auth/react';
+import Welcome from '../components/dashboard/welcomePannel/Welcome';
 
 const DashboardPage = () => {
     const { data: session } = useSession();
-        return (
-            <PrivateRoute>
-                <div className="bg-blue-100 min-h-screen flex">
-                <Sidebar />
-                <div className="flex-1 p-6">
-                    <h2 className="text-center font-semibold text-3xl text-blue-600">
-                        Welcome to {
-                            session?.user?.role === "admin" ? "Admin's dashboard":`${session?.user?.name}'s dashboard`
-                        }
-                    </h2>
-                    <div className="flex justify-center mt-4">
-                        <Image
-                            className="md:w-[1100px] md:h-[600px] rounded-lg"
-                            src="https://www.projectmanager.com/wp-content/uploads/2023/10/Project-Dashboard-Template-Excel-image.png"
-                            alt="Project Dashboard"
-                            width={1100}
-                            height={600}
-                        />
-                    </div>
-                    <div className="container mx-auto">
+        return (  <PrivateRoute>
+            <div className=' bg-slate-950 md:flex md:flex-row md:justify-between'>
+           <div className='md:w-1/4'>
+               <Sidebar />
+           </div>
+           <div className='container mx-auto md:w-4/4 p-2'>
+         <Welcome></Welcome>
+         <div className="container mx-auto">
                          <Notification></Notification>
                     </div>
-                </div>
-            </div>
-            </PrivateRoute>
+           </div>
+       </div>
+          </PrivateRoute>
+           
         );
     }
 
