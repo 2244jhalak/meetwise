ff6760f80ad5a9912b4d6281720742589c1d75e7
oishee <oisheekhan90@gmail.com>
2024-10-24 09:33:53 +0600

    modify all user design

--- src/app/dashboard/allUsers/page.jsx
+++ src/app/dashboard/allUsers/page.jsx
@@ -6,13 +6,15 @@ import React from 'react';
 const page = () => {
     return (
         <PrivateRoute>
-            <div>
-            <Sidebar></Sidebar>
-            <div className='pl-[270px] mt-10'>
-                <AllUsers></AllUsers>
-            </div>
-        </div>
-        </PrivateRoute>
+        <div className=' bg-slate-950 min-h-screen md:flex md:flex-row md:justify-between'>
+       <div className='md:w-1/4'>
+           <Sidebar />
+       </div>
+       <div className='container mx-auto md:w-4/4 p-2'>
+      <AllUsers></AllUsers>
+       </div>
+   </div>
+      </PrivateRoute>
     );
 };
 
