2350deb276c4f61c535c6e83642773c72c664297
Nakib Hasan Jhalak <103098230+2244jhalak@users.noreply.github.com>
2024-10-24 17:51:47 +0600

    Update page.jsx

--- src/app/dashboard/allMeetings/page.jsx
+++ src/app/dashboard/allMeetings/page.jsx
@@ -23,6 +23,7 @@ const Page = () => {
                     <AllMeetings></AllMeetings>
                 </div>
             </div>
+                 </div>
                 </AdminRoute>
             </PrivateRoute>
         );
