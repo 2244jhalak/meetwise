cf344a07a4124b4a80c73740b99df537b532cdc9
HIMU005 <hashanuzzaman99@gamil.com>
2024-10-24 12:42:18 +0600

    show the upcoming meeting meeting details in table formate

--- src/app/components/dashboard/createMeeting/MeetingScheduled/UpComingTab.jsx
+++ src/app/components/dashboard/createMeeting/MeetingScheduled/UpComingTab.jsx
@@ -1,10 +1,11 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useEffect, useState } from "react";
+import BookingTableRow from "./BookingTableRow";
 
 const UpComingTab = () => {
     const sortingType = '1';
     const today = new Date();
-    const [meeting, setMeeting] = useState([]);
+    const [meetings, setMeetings] = useState([]);
     const formattedDate = `${today.getDate().toString().padStart(2, '0')}/${(today.getMonth() + 1).toString().padStart(2, '0')}/${today.getFullYear()}`;
     const formattedTime = `${today.getHours().toString().padStart(2, '0')}:${today.getMinutes().toString().padStart(2, '0')}`;
 
@@ -26,17 +27,48 @@ const UpComingTab = () => {
                 }
 
                 const data = await response.json();
-                setMeeting(data);
+                setMeetings(data);
             } catch (error) {
-                console.error('Fetch error:', error);
+                // console.error('Fetch error:', error);
             }
         };
         fetchBookingData();
     }, [formattedDate, formattedTime, sortingType]);
     return (
-        <div>
-            upcoming content here
-        </div>
+        <table class="w-full divide-y divide-gray-200 dark:divide-gray-700 overflow-x-auto">
+            <thead class="bg-gray-50 dark:bg-gray-800">
+                <tr>
+                    <th scope="col" class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        Meeting Name
+                    </th>
+
+                    <th scope="col" class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        User Name
+                    </th>
+                    <th scope="col" class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        User Email
+                    </th>
+
+                    <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        Location
+                    </th>
+
+                    <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Link</th>
+
+                    <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Date</th>
+
+                    <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Time</th>
+                </tr>
+            </thead>
+
+            {/* table body here   */}
+            <tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                {
+                    meetings.map(meeting => <BookingTableRow key={meeting._id} meeting={meeting} />)
+                }
+            </tbody>
+
+        </table>
     );
 };
 
