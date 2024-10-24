17e3f66481119e436ce3f314febe5c819b90d940
HIMU005 <hashanuzzaman99@gamil.com>
2024-10-24 11:17:04 +0600

    upcoming meeting data is applied on the frontEnd

--- src/app/components/dashboard/createMeeting/MeetingScheduled/UpComingTab.jsx
+++ src/app/components/dashboard/createMeeting/MeetingScheduled/UpComingTab.jsx
@@ -1,5 +1,35 @@
 
 const UpComingTab = () => {
+    const sortingType = '1';
+    const today = new Date();
+    const formattedDate = `${today.getDate().toString().padStart(2, '0')}/${(today.getMonth() + 1).toString().padStart(2, '0')}/${today.getFullYear()}`;
+    const formattedTime = `${today.getHours().toString().padStart(2, '0')}:${today.getMinutes().toString().padStart(2, '0')}`;
+
+    const sortData = { sortingType, formattedDate, formattedTime };
+
+    useEffect(() => {
+        const fetchBookingData = async () => {
+            const queryParams = new URLSearchParams(sortData);
+            try {
+                const response = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_URL}/dashboard/scheduledMeeting/api?${queryParams.toString()}`, {
+                    method: 'GET',
+                    headers: {
+                        'Cache-Control': 'no-cache',
+                    },
+                });
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const data = await response.json();
+                console.log('Fetched data:', data);
+            } catch (error) {
+                console.error('Fetch error:', error);
+            }
+        };
+        fetchBookingData();
+    }, [formattedDate, formattedTime, sortingType]);
     return (
         <div>
             upcoming content here
