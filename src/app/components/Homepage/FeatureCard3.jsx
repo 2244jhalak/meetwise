8857a727b3aba98f11a5418a85321cc5cb36db53
Anamul029 <anamhaque.5101@gmail.com>
2024-10-25 01:58:15 +0600

    modify some component base style

---
+++ src/app/components/Homepage/FeatureCard3.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const FeatureCard3 = () => {
+    return (
+        <div>
+             <div className="card-body">
+                            <h2 className="card-title">Meeting Library</h2>
+                            <p>
+                            The All Meetings section displays a list of scheduled meetings, including their title, date, and time.    </p>
+
+                        </div>
+        </div>
+    );
+};
+
+export default FeatureCard3;
---
+++ src/app/components/Homepage/FeatureCard6.jsx
@@ -0,0 +1,16 @@
+
+const FeatureCard6 = () => {
+    return (
+        <div>
+            <div className="card-body">
+                <h2 className="card-title">Selecting Meeting Type</h2>
+                <p>
+                    Enable users to select different types of meetings such as one-on-one, group meetings, or webinars.,
+                </p>
+
+            </div>
+        </div>
+    );
+};
+
+export default FeatureCard6;
